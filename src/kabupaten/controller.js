ba5762ed3d27e89445ce8965d13372db78a13725
Muhammad Subhan <subhan12tiga45@gmail.com>
2024-02-04 12:43:35 +0700

    update

--- src/kabupaten/controller.js
+++ src/kabupaten/controller.js
@@ -17,35 +17,35 @@ module.exports = {
   },
 
   generate: async (req, res) => {
+    // try {
+    // const responseProvinsi = await axios.get('https://dev.farizdotid.com/api/daerahindonesia/provinsi');
+    // const provinsi = responseProvinsi.data.provinsi;
+    // console.log(provinsi);
+
+    // for (let i = 0; i < provinsi.length; i++) {
+    //   const idProvinsi = provinsi[i].id;
+
     try {
-      const responseProvinsi = await axios.get('https://dev.farizdotid.com/api/daerahindonesia/provinsi');
-      const provinsi = responseProvinsi.data.provinsi;
-      // console.log(provinsi);
+      const responseKota = await axios.get(`https://dev.farizdotid.com/api/daerahindonesia/kota?id_provinsi=35`);
+      const kota = responseKota.data.kota_kabupaten;
+      // console.log(kota);
+      for (let k = 0; k < kota.length; k++) {
+        const idKota = kota[k].id;
+        const namaKota = kota[k].nama;
 
-      for (let i = 0; i < provinsi.length; i++) {
-        const idProvinsi = provinsi[i].id;
-
-        try {
-          const responseKota = await axios.get(`https://dev.farizdotid.com/api/daerahindonesia/kota?id_provinsi=35`);
-          const kota = responseKota.data.kota_kabupaten;
-          // console.log(kota);
-          for (let k = 0; k < kota.length; k++) {
-            const idKota = kota[k].id;
-            const namaKota = kota[k].nama;
-
-            await Kabupaten.create({
-              id: idKota,
-              nama: namaKota,
-            });
-          }
-        } catch (err) {
-          console.error(`Error fetching city data for province with id ${idProvinsi}:`, err.message);
-        }
+        await Kabupaten.create({
+          id: idKota,
+          nama: namaKota,
+        });
       }
-
-      res.status(200).json({ message: 'Data has been successfully generated.' });
     } catch (err) {
-      res.status(500).json({ error: 'Internal Server Error' });
+      console.error(`Error fetching city data for province with id ${idProvinsi}:`, err.message);
     }
+    // }
+
+    //   res.status(200).json({ message: 'Data has been successfully generated.' });
+    // } catch (err) {
+    //   res.status(500).json({ error: 'Internal Server Error' });
+    // }
   },
 };
