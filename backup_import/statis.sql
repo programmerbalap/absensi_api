2bad06897a1a20d8153ed512f1a9e284629ab15e
Muhammad Subhan <subhan12tiga45@gmail.com>
2024-02-04 13:19:58 +0700

    update

--- backup_import/statis.sql
+++ backup_import/statis.sql
@@ -27,17 +27,17 @@ SET time_zone = "+00:00";
 -- Struktur dari tabel `statis`
 --
 
-CREATE TABLE `statis` (
-  `id` int(11) NOT NULL,
-  `jam_start` time NOT NULL,
-  `jam_end` time NOT NULL,
-  `min_efektif_start` int(11) NOT NULL,
-  `shift` varchar(20) NOT NULL,
-  `nominal_bonus` float NOT NULL,
-  `aktif` tinyint(1) NOT NULL DEFAULT 0,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- CREATE TABLE `statis` (
+--   `id` int(11) NOT NULL,
+--   `jam_start` time NOT NULL,
+--   `jam_end` time NOT NULL,
+--   `min_efektif_start` int(11) NOT NULL,
+--   `shift` varchar(20) NOT NULL,
+--   `nominal_bonus` float NOT NULL,
+--   `aktif` tinyint(1) NOT NULL DEFAULT 0,
+--   `createdAt` datetime NOT NULL,
+--   `updatedAt` datetime NOT NULL
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `statis`
@@ -54,8 +54,8 @@ INSERT INTO `statis` (`id`, `jam_start`, `jam_end`, `min_efektif_start`, `shift`
 --
 -- Indeks untuk tabel `statis`
 --
-ALTER TABLE `statis`
-  ADD PRIMARY KEY (`id`);
+-- ALTER TABLE `statis`
+--   ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
@@ -64,9 +64,9 @@ ALTER TABLE `statis`
 --
 -- AUTO_INCREMENT untuk tabel `statis`
 --
-ALTER TABLE `statis`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
+-- ALTER TABLE `statis`
+--   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+-- COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
