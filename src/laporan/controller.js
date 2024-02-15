9f5e9a3392d3e980bba8fd56c482e1db3ea77164
Muhammad Subhan <subhan12tiga45@gmail.com>
2024-02-15 10:38:18 +0700

    update

--- src/laporan/controller.js
+++ src/laporan/controller.js
@@ -24,128 +24,128 @@ module.exports = {
           'no_nik',
           'nama',
           'jk',
-          [
-            Sequelize.literal(`(
-                SELECT GROUP_CONCAT(DISTINCT a.kemampuan SEPARATOR ', ')
-                FROM absensi AS a
-                WHERE a.uuid_karyawan = karyawan.uuid 
-                AND a.hadir = 'Hadir' 
-                AND YEAR(a.tanggal) = ${year} AND MONTH(a.tanggal) = ${month})`),
-            'kemampuan',
-          ],
-          [
-            Sequelize.literal(`(
-                SELECT COUNT(DISTINCT a.tanggal) 
-                FROM absensi AS a
-                WHERE a.hadir = 'Hadir' 
-                AND YEAR(a.tanggal) = ${year} AND MONTH(a.tanggal) = ${month})`),
-            'efektif_kerja',
-          ],
-          [
-            Sequelize.literal(`(
-                SELECT COUNT(DISTINCT a.tanggal) 
-                FROM absensi AS a
-                WHERE a.uuid_karyawan = karyawan.uuid 
-                AND a.hadir = 'Hadir' 
-                AND YEAR(a.tanggal) = ${year} AND MONTH(a.tanggal) = ${month})`),
-            'hadir',
-          ],
-          [
-            Sequelize.literal(`(
-                SELECT COUNT(DISTINCT a.tanggal)
-                FROM absensi AS a
-                WHERE a.uuid_karyawan = karyawan.uuid
-                AND a.hadir = 'Tidak Hadir'
-                AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month} )`),
-            'tidak',
-          ],
-          [
-            Sequelize.literal(`(
-                SELECT COUNT(DISTINCT a.tanggal)
-                FROM absensi AS a
-                WHERE a.uuid_karyawan = karyawan.uuid
-                AND a.hadir = 'Hadir'
-                AND a.bonus = true
-                AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month}
-             )`),
-            'bonus',
-          ],
-          [
-            Sequelize.literal(`(
-                SELECT COUNT(DISTINCT a.tanggal)
-                FROM absensi AS a
-                WHERE a.uuid_karyawan = karyawan.uuid
-                AND a.hadir = 'Hadir'
-                AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month}
-                AND a.shift = 'Normal')`),
-            'normal',
-          ],
-          [
-            Sequelize.literal(`(
-                SELECT COUNT(DISTINCT a.tanggal)
-                FROM absensi AS a
-                WHERE a.uuid_karyawan = karyawan.uuid
-                AND a.hadir = 'Hadir'
-                AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month}
-                AND a.shift = 'Lembur')`),
-            'lembur',
-          ],
-          [
-            Sequelize.literal(`(
-                SELECT ROUND(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end) * a.nominal_gaji / 60))
-                FROM absensi AS a
-                WHERE a.uuid_karyawan = karyawan.uuid
-                AND a.hadir = 'Hadir'
-                AND YEAR(a.tanggal) = ${year} AND MONTH(a.tanggal) = ${month}
-                AND a.shift = 'Normal')`),
-            'gaji_normal',
-          ],
-          [
-            Sequelize.literal(`(
-                SELECT
-                  CASE
-                    WHEN SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) > 60 THEN (( FLOOR(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) / 60) * 2 * a.nominal_gaji )  - (a.nominal_gaji * 0.5) ) + (ROUND (SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) % 60) * 2 * a.nominal_gaji / 60)
-                    ELSE ROUND(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end) * a.nominal_gaji / 60 * 1.5))
-                  END
-                FROM absensi AS a
-                WHERE a.uuid_karyawan = karyawan.uuid
-                AND a.hadir = 'Hadir'
-                AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month}
-                AND a.shift = 'Lembur')`),
-            'gaji_lembur',
-          ],
-          [
-            Sequelize.literal(`(
-              SELECT a.nominal_bonus
-              FROM absensi AS a
-              WHERE a.uuid_karyawan = karyawan.uuid 
-              AND a.hadir = 'Hadir' 
-              AND YEAR(a.tanggal) = ${year} AND MONTH(a.tanggal) = ${month}
-              GROUP BY a.tanggal
-              LIMIT 1
-            )`),
-            'gaji_bonus',
-          ],
-          [
-            Sequelize.literal(`(
-                SELECT TIME_FORMAT(SEC_TO_TIME(ROUND(SUM(TIMESTAMPDIFF(SECOND, a.time_start, a.time_end)))), '%H:%i:%s')
-                FROM absensi AS a
-                WHERE a.uuid_karyawan = karyawan.uuid
-                AND a.hadir = 'Hadir'
-                AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month}
-                AND a.shift = 'Lembur')`),
-            'total_durasi_lembur',
-          ],
-          [
-            Sequelize.literal(`(
-                SELECT TIME_FORMAT(SEC_TO_TIME(ROUND(SUM(TIMESTAMPDIFF(SECOND, a.time_start, a.time_end)))), '%H:%i:%s')
-                FROM absensi AS a
-                WHERE a.uuid_karyawan = karyawan.uuid
-                AND a.hadir = 'Hadir'
-                AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month}
-                AND a.shift = 'Normal' )`),
-            'total_durasi_normal',
-          ],
+          // [
+          //   Sequelize.literal(`(
+          //       SELECT GROUP_CONCAT(DISTINCT a.kemampuan SEPARATOR ', ')
+          //       FROM absensi AS a
+          //       WHERE a.uuid_karyawan = karyawan.uuid
+          //       AND a.hadir = 'Hadir'
+          //       AND YEAR(a.tanggal) = ${year} AND MONTH(a.tanggal) = ${month})`),
+          //   'kemampuan',
+          // ],
+          // [
+          //   Sequelize.literal(`(
+          //       SELECT COUNT(DISTINCT a.tanggal)
+          //       FROM absensi AS a
+          //       WHERE a.hadir = 'Hadir'
+          //       AND YEAR(a.tanggal) = ${year} AND MONTH(a.tanggal) = ${month})`),
+          //   'efektif_kerja',
+          // ],
+          // [
+          //   Sequelize.literal(`(
+          //       SELECT COUNT(DISTINCT a.tanggal)
+          //       FROM absensi AS a
+          //       WHERE a.uuid_karyawan = karyawan.uuid
+          //       AND a.hadir = 'Hadir'
+          //       AND YEAR(a.tanggal) = ${year} AND MONTH(a.tanggal) = ${month})`),
+          //   'hadir',
+          // ],
+          // [
+          //   Sequelize.literal(`(
+          //       SELECT COUNT(DISTINCT a.tanggal)
+          //       FROM absensi AS a
+          //       WHERE a.uuid_karyawan = karyawan.uuid
+          //       AND a.hadir = 'Tidak Hadir'
+          //       AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month} )`),
+          //   'tidak',
+          // ],
+          // [
+          //   Sequelize.literal(`(
+          //       SELECT COUNT(DISTINCT a.tanggal)
+          //       FROM absensi AS a
+          //       WHERE a.uuid_karyawan = karyawan.uuid
+          //       AND a.hadir = 'Hadir'
+          //       AND a.bonus = true
+          //       AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month}
+          //    )`),
+          //   'bonus',
+          // ],
+          // [
+          //   Sequelize.literal(`(
+          //       SELECT COUNT(DISTINCT a.tanggal)
+          //       FROM absensi AS a
+          //       WHERE a.uuid_karyawan = karyawan.uuid
+          //       AND a.hadir = 'Hadir'
+          //       AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month}
+          //       AND a.shift = 'Normal')`),
+          //   'normal',
+          // ],
+          // [
+          //   Sequelize.literal(`(
+          //       SELECT COUNT(DISTINCT a.tanggal)
+          //       FROM absensi AS a
+          //       WHERE a.uuid_karyawan = karyawan.uuid
+          //       AND a.hadir = 'Hadir'
+          //       AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month}
+          //       AND a.shift = 'Lembur')`),
+          //   'lembur',
+          // ],
+          // [
+          //   Sequelize.literal(`(
+          //       SELECT ROUND(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end) * a.nominal_gaji / 60))
+          //       FROM absensi AS a
+          //       WHERE a.uuid_karyawan = karyawan.uuid
+          //       AND a.hadir = 'Hadir'
+          //       AND YEAR(a.tanggal) = ${year} AND MONTH(a.tanggal) = ${month}
+          //       AND a.shift = 'Normal')`),
+          //   'gaji_normal',
+          // ],
+          // [
+          //   Sequelize.literal(`(
+          //       SELECT
+          //         CASE
+          //           WHEN SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) > 60 THEN (( FLOOR(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) / 60) * 2 * a.nominal_gaji )  - (a.nominal_gaji * 0.5) ) + (ROUND (SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) % 60) * 2 * a.nominal_gaji / 60)
+          //           ELSE ROUND(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end) * a.nominal_gaji / 60 * 1.5))
+          //         END
+          //       FROM absensi AS a
+          //       WHERE a.uuid_karyawan = karyawan.uuid
+          //       AND a.hadir = 'Hadir'
+          //       AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month}
+          //       AND a.shift = 'Lembur')`),
+          //   'gaji_lembur',
+          // ],
+          // [
+          //   Sequelize.literal(`(
+          //     SELECT a.nominal_bonus
+          //     FROM absensi AS a
+          //     WHERE a.uuid_karyawan = karyawan.uuid
+          //     AND a.hadir = 'Hadir'
+          //     AND YEAR(a.tanggal) = ${year} AND MONTH(a.tanggal) = ${month}
+          //     GROUP BY a.tanggal
+          //     LIMIT 1
+          //   )`),
+          //   'gaji_bonus',
+          // ],
+          // [
+          //   Sequelize.literal(`(
+          //       SELECT TIME_FORMAT(SEC_TO_TIME(ROUND(SUM(TIMESTAMPDIFF(SECOND, a.time_start, a.time_end)))), '%H:%i:%s')
+          //       FROM absensi AS a
+          //       WHERE a.uuid_karyawan = karyawan.uuid
+          //       AND a.hadir = 'Hadir'
+          //       AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month}
+          //       AND a.shift = 'Lembur')`),
+          //   'total_durasi_lembur',
+          // ],
+          // [
+          //   Sequelize.literal(`(
+          //       SELECT TIME_FORMAT(SEC_TO_TIME(ROUND(SUM(TIMESTAMPDIFF(SECOND, a.time_start, a.time_end)))), '%H:%i:%s')
+          //       FROM absensi AS a
+          //       WHERE a.uuid_karyawan = karyawan.uuid
+          //       AND a.hadir = 'Hadir'
+          //       AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month}
+          //       AND a.shift = 'Normal' )`),
+          //   'total_durasi_normal',
+          // ],
         ],
         where: {
           [Op.and]: [{ id_jabatan: [3, 4] }, { status_karyawan: 'Aktif' }],
