57b9905cc006c8d2115c21a28592000c038e6748
Muhammad Subhan <subhan12tiga45@gmail.com>
2024-02-15 16:17:12 +0700

    update

--- src/laporan/controller.js
+++ src/laporan/controller.js
@@ -105,16 +105,7 @@ module.exports = {
                 SELECT
                   CASE
                     WHEN SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) > 60 THEN
-                      (
-                        (
-                          FLOOR(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) / 60) * 2 * a.nominal_gaji
-                        ) - (a.nominal_gaji * 0.5)
-                      ) + (
-                        (
-                          SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end) % 60)
-                        ) * 2 * a.nominal_gaji / 60
-                      )
-                      
+                      FLOOR(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) / 60) * 2 * a.nominal_gaji
                     ELSE
                       ROUND(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end) * a.nominal_gaji / 60 * 1.5))
                   END
@@ -125,6 +116,17 @@ module.exports = {
                 AND a.shift = 'Lembur')`),
             'gaji_lembur',
           ],
+
+          // (
+          //   (
+          //     FLOOR(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) / 60) * 2 * a.nominal_gaji
+          //   ) - (a.nominal_gaji * 0.5)
+          // ) + (
+          //   (
+          //     SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) % 60
+          //   ) * 2 * a.nominal_gaji / 60
+          // )
+
           // [
           //   Sequelize.literal(`(
           //       SELECT
