71aaeb9c450540e98c750e593cca942d1e332a92
Muhammad Subhan <subhan12tiga45@gmail.com>
2024-02-15 16:12:50 +0700

    update

--- src/laporan/controller.js
+++ src/laporan/controller.js
@@ -105,9 +105,17 @@ module.exports = {
                 SELECT
                   CASE
                     WHEN SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) > 60 THEN
-                    (( FLOOR(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) / 60) * 2 * a.nominal_gaji) - (a.nominal_gaji * 0.5) ) + (ROUND (SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) % 60) * 2 * a.nominal_gaji / 60)
+                      (
+                        (
+                          FLOOR(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) / 60) * 2 * a.nominal_gaji
+                        ) - (a.nominal_gaji * 0.5)
+                      ) + (
+                        (
+                          SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) % 60
+                        ) * 2 * a.nominal_gaji / 60
+                      )                    
                     ELSE
-                    ROUND(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end) * a.nominal_gaji / 60 * 1.5))
+                      ROUND(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end) * a.nominal_gaji / 60 * 1.5))
                   END
                 FROM absensi AS a
                 WHERE a.uuid_karyawan = karyawan.uuid
