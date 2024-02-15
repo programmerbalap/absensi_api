2079f5fcd489a1b1b1265a9897211f4593cbf0b6
Muhammad Subhan <subhan12tiga45@gmail.com>
2024-02-15 13:58:54 +0700

    update

--- src/laporan/controller.js
+++ src/laporan/controller.js
@@ -100,20 +100,20 @@ module.exports = {
                 AND a.shift = 'Normal')`),
             'gaji_normal',
           ],
-          [
-            Sequelize.literal(`(
-                SELECT
-                  CASE
-                    WHEN SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) > 60 THEN (( FLOOR(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) / 60) * 2 * a.nominal_gaji )  - (a.nominal_gaji * 0.5) ) + (ROUND (SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) % 60) * 2 * a.nominal_gaji / 60)
-                    ELSE ROUND(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end) * a.nominal_gaji / 60 * 1.5))
-                  END
-                FROM absensi AS a
-                WHERE a.uuid_karyawan = karyawan.uuid
-                AND a.hadir = 'Hadir'
-                AND YEAR(a.tanggal) = ${year} AND MONTH(a.tanggal) = ${month}
-                AND a.shift = 'Lembur')`),
-            'gaji_lembur',
-          ],
+          // [
+          //   Sequelize.literal(`(
+          //       SELECT
+          //         CASE
+          //           WHEN SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) > 60 THEN (( FLOOR(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) / 60) * 2 * a.nominal_gaji )  - (a.nominal_gaji * 0.5) ) + (ROUND (SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) % 60) * 2 * a.nominal_gaji / 60)
+          //           ELSE ROUND(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end) * a.nominal_gaji / 60 * 1.5))
+          //         END
+          //       FROM absensi AS a
+          //       WHERE a.uuid_karyawan = karyawan.uuid
+          //       AND a.hadir = 'Hadir'
+          //       AND YEAR(a.tanggal) = ${year} AND MONTH(a.tanggal) = ${month}
+          //       AND a.shift = 'Lembur')`),
+          //   'gaji_lembur',
+          // ],
           // [
           //   Sequelize.literal(`(
           //     SELECT
@@ -220,15 +220,15 @@ module.exports = {
           AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month})`),
             'hadir',
           ],
-          // [
-          //   Sequelize.literal(`(
-          // SELECT COUNT(DISTINCT a.tanggal)
-          // FROM absensi AS a
-          // WHERE a.uuid_karyawan = Karyawan.uuid
-          // AND a.hadir = 'Tidak Hadir'
-          // AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month})`),
-          //   'tidak',
-          // ],
+          [
+            Sequelize.literal(`(
+          SELECT COUNT(DISTINCT a.tanggal)
+          FROM absensi AS a
+          WHERE a.uuid_karyawan = Karyawan.uuid
+          AND a.hadir = 'Tidak Hadir'
+          AND YEAR(a.tanggal) = ${year}  AND MONTH(a.tanggal) = ${month})`),
+            'tidak',
+          ],
         ],
         where: {
           [Op.and]: [{ id_jabatan: [3, 4] }, { status_karyawan: 'Aktif' }],
