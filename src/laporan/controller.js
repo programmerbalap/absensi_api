0f707f45bc3a8a3feae3a54a449ee0c018703668
Muhammad Subhan <subhan12tiga45@gmail.com>
2024-02-15 16:34:33 +0700

    update

--- src/laporan/controller.js
+++ src/laporan/controller.js
@@ -105,9 +105,10 @@ module.exports = {
                 SELECT
                   CASE
                     WHEN SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) > 60 THEN
-                      ROUND((SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end) * a.nominal_gaji / 60)) * 2)  - (a.nominal_gaji * 0.5)
-                    ELSE
                       ROUND(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end) * a.nominal_gaji / 60 * 1.5))
+                    ELSE
+                      ROUND(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end) * a.nominal_gaji / 60)) * 2 +
+                      ROUND (SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) % 60) * 2 * a.nominal_gaji / 60
                   END
                 FROM absensi AS a
                 WHERE a.uuid_karyawan = karyawan.uuid
@@ -132,7 +133,7 @@ module.exports = {
           //       SELECT
           //         CASE
           //           WHEN SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) > 60 THEN
-          //             (( FLOOR(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) / 60) * 2 * a.nominal_gaji) - (a.nominal_gaji * 0.5) ) + (ROUND (SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) % 60) * 2 * a.nominal_gaji / 60)
+          //             (( FLOOR(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) / 60) * 2 * a.nominal_gaji) - (a.nominal_gaji * 0.5) ) + (ROUND (SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end)) % 60) * 2 * a.nominal_gaji / 60))
           //           ELSE
           //             ROUND(SUM(TIMESTAMPDIFF(MINUTE, a.time_start, a.time_end) * a.nominal_gaji / 60 * 1.5))
           //         END
